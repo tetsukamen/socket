f1d1cb6f5611df71b93d3e92002c0fbb09f38543
Tetsuya Wada <tetsukamen00@gmail.com>
2021-12-14 16:48:02 +0900

    連続して動作

--- udp/client.c
+++ udp/client.c
@@ -1,23 +1,42 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
-int
-main()
+#define SERVER_ADDR "127.0.0.1"
+#define SERVER_PORT 8080
+#define BUF_SIZE 1024
+
+int main()
 {
- int sock;
- struct sockaddr_in addr;
+   int sock;
+   struct sockaddr_in addr;
+   char send_buf[BUF_SIZE], recv_buf;
 
- sock = socket(AF_INET, SOCK_DGRAM, 0);
+   sock = socket(AF_INET, SOCK_DGRAM, 0);
 
- addr.sin_family = AF_INET;
- addr.sin_port = htons(12345);
- addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+   addr.sin_family = AF_INET;
+   addr.sin_port = htons(SERVER_PORT);
+   addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
 
- sendto(sock, "HELLO", 5, 0, (struct sockaddr *)&addr, sizeof(addr));
+   printf("Input Message...\n");
+   while (1)
+   {
+      /* サーバーに送る文字列を取得 */
+      scanf("%s", send_buf);
+      if (strcmp(send_buf, "finish") != 0)
+      {
+         sendto(sock, send_buf, 5, 0, (struct sockaddr *)&addr, sizeof(addr));
+      }
+      else
+      {
+         break;
+      }
+   }
 
- close(sock);
+   close(sock);
 
- return 0;
+   return 0;
 }
--- udp/server.c
+++ udp/server.c
@@ -5,25 +5,32 @@
 #include <string.h>
 #include <unistd.h>
 
+#define SERVER_PORT 8080
+#define BUF_SIZE 1024
+
 int main()
 {
     int sock;
     struct sockaddr_in addr;
 
-    char buf[2048];
+    char buf[BUF_SIZE];
 
     sock = socket(AF_INET, SOCK_DGRAM, 0);
 
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(12345);
+    addr.sin_port = htons(SERVER_PORT);
     addr.sin_addr.s_addr = INADDR_ANY;
 
     bind(sock, (struct sockaddr *)&addr, sizeof(addr));
 
     memset(buf, 0, sizeof(buf));
-    recv(sock, buf, sizeof(buf), 0);
 
-    printf("%s\n", buf);
+    printf("Listening...\n");
+    while (1)
+    {
+        recv(sock, buf, sizeof(buf), 0);
+        printf("%s\n", buf);
+    }
 
     close(sock);
 
