3911335f9e6883d9bbee1d66a12ec27054b254ae
Tetsuya Wada <tetsukamen00@gmail.com>
2022-01-19 13:06:35 +0900

    convert uint8_t to char

--- coap-simple/test.cpp
+++ coap-simple/test.cpp
@@ -11,28 +11,54 @@
 #include "SHA256.h"
 
 int main() {
-  const char* message = "133.1.1.5aaaaaaaaaaaaaaaaa";
+  // const char* message = "133.1.1.5aaaaaaaaaaaaaaaaa";
 
-  //	結果格納配列を作成する
-  unsigned int H[INIT_HASH_LENGTH];
+  // //	結果格納配列を作成する
+  // unsigned int H[INIT_HASH_LENGTH];
 
-  //	SHA256インスタンスを作成
-  SHA256 sha256;
+  // //	SHA256インスタンスを作成
+  // SHA256 sha256;
 
-  //	パディング処理を実行
-  unsigned char** result = sha256.padding((char*)message);
+  // //	パディング処理を実行
+  // unsigned char** result = sha256.padding((char*)message);
 
-  //	ブロックを表示
-  // sha256.print_block(result);
+  // //	ブロックを表示
+  // // sha256.print_block(result);
 
-  //	ハッシュ化を行う
-  sha256.compute(result, H);
+  // //	ハッシュ化を行う
+  // sha256.compute(result, H);
 
-  //	結果を出力する
-  sha256.print_hash(H);
+  // //	結果を出力する
+  // sha256.print_hash(H);
 
-  //	メモリ開放
-  sha256.free_block(result);
+  // //	メモリ開放
+  // sha256.free_block(result);
+
+  uint8_t a[10] = "aaaa";
+  a[1] = 0x98;
+  a[2] = 0x11;
+  char b[10];
+  for (int i = 0; i < 10; i++) {
+    b[i] = (char)(a[i] - 128);
+  }
+
+  printf("a:");
+  for (int i = 0; i < 10; i++) {
+    printf("%#x ", a[i]);
+  }
+  printf("\n");
+
+  printf("b:");
+  for (int i = 0; i < 10; i++) {
+    printf("%#x ", b[i]);
+  }
+  printf("\n");
+
+  printf("b+128:");
+  for (int i = 0; i < 10; i++) {
+    printf("%#x ", b[i] + 128);
+  }
+  printf("\n");
 
   return 0;
 }
